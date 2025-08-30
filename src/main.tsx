9bfe695e9d418d534248984eacc4abb86bd3eb0a
Belyse Ingabire <your-email@example.com>
2025-08-30 10:00:56 +0200

    updated login page

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-// import { ProductProvider } from './context/ProductContext.tsx';
-// import { CartProvider } from './context/CartContext.tsx';
 import { AuthProvider } from './context/AuthContext.tsx';
 
 const root = createRoot(document.getElementById('root')!);
@@ -11,11 +9,7 @@ const root = createRoot(document.getElementById('root')!);
 root.render(
   <StrictMode>
     <AuthProvider>
-      {/* <CartProvider>
-        <ProductProvider> */}
           <App />
-        {/* </ProductProvider>
-      </CartProvider> */}
     </AuthProvider>
   </StrictMode>
 );
